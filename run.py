9202a0ff13c2b3e227e964357da9c1ea1c85f934
Eric Rincon <eric.rincon147@gmail.com>
2019-03-04 23:40:15 -0600

    Refactored args boolean

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from config import Config
 
 from geopy.geocoders import Nominatim
 
+from host import argparser_utils
+
 
 def get_location(location_query):
     geolocator = Nominatim()
@@ -12,21 +14,10 @@ def get_location(location_query):
     return geolocator.geocode(location_query)
 
 
-def boolean(arg):
-    arg = str(arg)
-
-    arg = arg.lower()
-
-    if arg in ("y", "t", "yes", "1", "true"):
-        return True
-    else:
-        return False
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--browser", default="firefox")
-    parser.add_argument("--debug", default="false", type=boolean)
+    parser.add_argument("--debug", default="false", type=argparser_utils.boolean)
     args = parser.parse_args()
 
     while True:
