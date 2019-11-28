f2c65866ab1b7fd9e01f6053e1a54d8198182abb
Eric Rincon <eric.rincon147@gmail.com>
2019-11-27 18:21:13 -0600

    Added custom port and host

--- swiper/app.py
+++ swiper/app.py
@@ -1,7 +1,15 @@
+import argparse
 from swiper import app
 
+
 def main():
-    app.run(debug=True, port=5002)
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--host", default="0.0.0.0")
+    arg_parser.add_argument("--port", default=5000, type=int)
+    arg_parser.add_argument("--debug", default=1, type=int)
+    args = arg_parser.parse_args()
+
+    app.run(debug=args.debug == 1, host=args.host,  port=args.port)
 
 
 if __name__ == '__main__':
