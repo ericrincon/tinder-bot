e6c5c650601c8d34f01d4eb75b4d60f23c0d373a
Eric Rincon <eric.rincon147@gmail.com>
2020-06-12 16:36:59 -0500

    Fixed country being captilized

--- host/host/utils/location.py
+++ host/host/utils/location.py
@@ -3,7 +3,8 @@ import geocoder
 from typing import Dict
 
 
-def get_location() -> Dict:
+def get_location() -> Dict[str, str]:
     location = geocoder.ip("me")
 
-    return {"state": location.state, "city": location.city, "country": location.country}
+    return {"state": location.state, "city": location.city,
+            "country": location.country}
